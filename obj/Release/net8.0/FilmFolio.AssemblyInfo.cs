52188ee1978a26ab2caca8f97c9449dae037ddb2
Artin Simonian <artin.simonian@outlook.com>
2024-08-04 10:25:43 -0700

    NavBar

--- obj/Release/net8.0/FilmFolio.AssemblyInfo.cs
+++ obj/Release/net8.0/FilmFolio.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmFolio")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8e8f9591f01c7b091017ef980fcb71fc677e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmFolio")]
